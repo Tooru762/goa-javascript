7f842aff1191cd4fd874a346bbc46643420b306d
Tooru762 <156686008+Tooru762@users.noreply.github.com>
2025-10-31 22:02:29 +0400

    Add files via upload

---
+++ day82/classwork/day82classwork.js
@@ -0,0 +1,13 @@
+//შექმენით ცვლადები var/let/const-ით და კონსოლში გამოიტანეთ.
+//შექმენით ცვლადი და alert-ში გამოიტანეთ ამ ცვლადის მნიშვნელობა 
+
+let Name ="luka" 
+console.log(Name)
+
+var Age = 16
+console.log(Age)
+
+const  surname = "wedia" 
+console.log(surname) 
+
+alert(surname) 
---
+++ day82/homework/day82.js
@@ -0,0 +1,66 @@
+//შექმენით ორი ცვლადი სადაც შეინახავთ რიცხვებს. კონსოლში გამოიტანეთ ამ ორი რიცხვის ჯამი.
+//შექმენით ცვლადი სადაც შეინახავთ თქვენს სახელს და alert-ში გამოიტანეთ "გამარჯობა {ცვლასის მნიშვნელობა}.
+//შექმენით ცვლადები let-ით const-ით და var-ით და კომენტარის სახით განმარტეთ თითოეული მათგანი. კონსოლში გამოიტანეთ ამ ცვლადის მნიშვნელობები
+//შექმენით ცვლადები let-ით const-ით და var-ით, თითოეული მათგანის მნიშვნელობა გაანახლეთ ბევრჯერ და კონსოლში გამოიტანეთ შედეგი. რაც არ იმუშავებს განმარტეთ რატომ არ მუშაობს.
+//შექმენით 2 ცვლადი სადაც შეინახავთ Number ტიპის მონაცემებს. კონსოლში გამოიტანეთ ამ ორ რიცხვზე შესრულებული ყველა მათემატიკური მოქმედება.
+//შექმენით ცვლადი რომლის თავდაპირველი მნიშვნელობა იქნება Number ტიპის მონაცემი, გაანახლეთ და შემდეგ Bool ტიპის მონაცემი შეინახეთ, შემდეგ string ტიპის და გამოიტანეთ შედეგი კონსოლში. ალერტში გამოიტანეთ ტექსტი "შეამიწმე კონსოლი"
+//შექმენით ცვლადი სადაც შეინახავთ რიცხვს და ამ ცვლადის მნიშვნელობა გაანახლეთ და გაზარდეთ 10-ით, შედეგი კონსოლში გამოიტანეთ. 
+
+//davaleba 1
+let Age = 16 
+let age = 17
+
+console.log(Age+age)   
+
+//davaleba 2
+let Name ="luka"  
+let a = "nika"
+
+alert("gamarjoba "+Name) 
+
+//davaleba 3
+//letit shegvizlia ganvanaxlot cvladi 
+//var aris letis zveli versia
+//constshi ar shegvizlia ganvanaxlot cvladi 
+
+let b = "letit shegvizlia ganvanaxlot cvladi" 
+var y = "vari aris letis zveli versia" 
+const n ="constshi ar shegvizlia ganvanaxlot cvladi" 
+console.log(b,y,n)  
+
+
+//davaleba 4
+let c ="luka" 
+c ="nika" 
+c ="saba" 
+c ="dato" 
+
+var m = 5
+m = 3
+m = 4 
+
+const g =1 
+//ar imushavebs
+console.log(c,m,g) 
+
+//davaleba 5
+let Number=5
+let number=3
+console.log(Number-number) 
+console.log(number+Number)
+console.log(Number/number) 
+console.log(Number*number)
+
+
+//davaleba 6 
+let Num=3
+Num=4 
+Num=true
+Num="sheamowme konsoli" 
+alert("sheamowme konsoli") 
+console.log(Num)  
+
+//davaleba 7
+let j=10
+j=20
+console.log(j) 
