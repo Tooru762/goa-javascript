aea5f6d7c881c98d97644a6fad084e86c12a01dc
Tooru762 <156686008+Tooru762@users.noreply.github.com>
2025-11-16 00:00:03 +0400

    Add files via upload

---
+++ day91/day91classwork.js
@@ -0,0 +1,17 @@
+
+let array = ["ვაშლი", "ბანანი", "მსხალი"];
+array.unshift("luka");
+console.log(array); 
+
+
+let Array = ["ვაშლი", "ბანანი", "მსხალი", "ატამი", "ყურძენი"];
+Array.push(14);
+console.log(Array);
+
+
+
+
+let aRray = ["ვაშლი", "ბანანი", "მსხალი", "ატამი", "მარწყვი"];
+aRray.splice(3, 0, "Goa");
+aRray.pop();
+console.log(aRray);
